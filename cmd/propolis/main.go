8680580304f683065d2709cd73522305de2ac05f
dsrtusr88 <33256205+dsrtusr88@users.noreply.github.com>
2024-03-03 14:30:30 -0500

    Update main.go

--- cmd/propolis/main.go
+++ cmd/propolis/main.go
@@ -1,38 +1,44 @@
 package main
 
 import (
-	"os"
-	"syscall"
+    "os"
+    "syscall"
 
-	"gitlab.com/catastrophic/assistance/logthis"
-
-	"gitlab.com/passelecasque/propolis"
+    "gitlab.com/catastrophic/assistance/logthis"
+    "gitlab.com/passelecasque/propolis"
 )
 
+type propolisArgs struct {
+    // Define the fields of propolisArgs struct here
+}
+
+// Define Version constant
+const Version = "1.0.0"
+
 func main() {
-	// checking external tools
-	if err := propolis.CheckExternalBinaries("sox", "flac"); err != nil {
-		logthis.Error(err, logthis.NORMAL)
-		return
-	}
+    // checking external tools
+    if err := propolis.CheckExternalBinaries("sox", "flac"); err != nil {
+        logthis.Error(err, logthis.NORMAL)
+        return
+    }
 
-	// parsing CLI
-	cli := &propolisArgs{}
-	if err := cli.parseCLI(os.Args[1:]); err != nil {
-		logthis.Error(err, logthis.NORMAL)
-		return
-	}
-	if cli.builtin {
-		return
-	}
+    // parsing CLI
+    cli := &propolisArgs{}
+    if err := cli.parseCLI(os.Args[1:]); err != nil {
+        logthis.Error(err, logthis.NORMAL)
+        return
+    }
+    if cli.builtin {
+        return
+    }
 
-	results, _, err := propolis.Run(cli.path, cli.metadataRoot, cli.disableSpecs, cli.disableCombinedSpecs, cli.problemsOnly, cli.snatched, cli.jsonOutput, true, Version)
-	if err != nil {
-		logthis.Error(err, logthis.NORMAL)
-	}
+    results, _, err := propolis.Run(cli.path, cli.metadataRoot, cli.disableSpecs, cli.disableCombinedSpecs, cli.problemsOnly, cli.snatched, cli.jsonOutput, true, Version)
+    if err != nil {
+        logthis.Error(err, logthis.NORMAL)
+    }
 
-	// returning nonzero exit status if something serious was found
-	if results.Errors != 0 {
-		syscall.Exit(1)
-	}
+    // returning nonzero exit status if something serious was found
+    if results.Errors != 0 {
+        syscall.Exit(1)
+    }
 }
